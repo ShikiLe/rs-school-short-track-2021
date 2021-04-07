9cc93191b72e4a56be16f26717383611f6bf7da5
Elena Karpovich <mariajane1897@gmail.com>
2021-04-07 16:12:35 +0300

    feat: add task 10 solution

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,20 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let low = 0;
+  let high = array.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((high + low) / 2);
+    if (array[mid] === value) {
+      return mid;
+    } else if (value > array[mid]) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return mid;
 }
 
 module.exports = findIndex;
