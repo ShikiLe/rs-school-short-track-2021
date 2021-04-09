23777950e9931ffee479bba4b1194289d3a87759
Elena Karpovich <mariajane1897@gmail.com>
2021-04-09 15:34:47 +0300

    feat: add 11 task solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,28 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const strArr = str.split('');
+  let count = 1;
+  let temp = '';
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] === strArr[i + 1]) {
+      count += 1;
+    }
+    if (strArr[i] !== strArr[i + 1]) {
+      temp += count.toString();
+      temp += strArr[i];
+      count = 1;
+    }
+  }
+  const tempArr = temp.split('');
+  let result = '';
+  tempArr.forEach((el) => {
+    if (el !== '1') {
+      result += el;
+    }
+  });
+  return result;
 }
 
 module.exports = encodeLine;
