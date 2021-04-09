51cdaa9afdaa571e1ab8dd4a3c2fb1318ac0e5c7
Elena Karpovich <mariajane1897@gmail.com>
2021-04-09 16:21:58 +0300

    feat: add 5 task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,21 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = Array.from(n.toString());
+  const sortedArr = arr.slice().sort(function (a, b) {
+    return a - b;
+  });
+  const result = [];
+  while (arr.length !== arr.length - 2) {
+    arr.forEach((el) => {
+      if (el !== sortedArr[0]) {
+        result.push(el);
+      }
+    });
+  }
+
+  return result.join('');
 }
 
 module.exports = deleteDigit;
